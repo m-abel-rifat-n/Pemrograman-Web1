8e13170b7185b8a2dc2eda80939039e58f8073ed
m-abel-rifat-n <80399877+m-abel-rifat-n@users.noreply.github.com>
2024-02-26 11:46:29 +0700

    Add files via upload

---
+++ Pertemuan 2/variabel.php
@@ -0,0 +1,11 @@
+<?php
+$name= "antok";
+define("GAJI", 500000000);
+
+echo $name;
+echo "<br>";
+echo GAJI;
+echo"<br";
+echo "$name bergaji rp.GAJI";
+echo "<br>";
+echo "$name bergaji rp. " .GAJI;
